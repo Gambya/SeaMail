e66f0edbab1138bf69d16ff9c8d77d596ebd3da9
Gambya <robertolopesramos@gmail.com>
2015-04-26 17:44:44 -0300

    First Commit

---
+++ SeaMail/SeaMail.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaMail
+{
+    public static class SeaMail
+    {
+        public static SmtpClient Client { get; set; }
+        public static void Send(String host, String user, String password, String from, String to, String subject, String message)
+        {
+            Client = new SmtpClient();
+            Client.Host = host;
+            Client.EnableSsl = true;
+            Client.Credentials = new NetworkCredential(user, password);
+            Client.Send(from, to, subject, message);
+        }
+
+        public static void Send(String host, String user, String password, String from, String[] to, String subject, String message)
+        {
+            Client = new SmtpClient();
+            Client.Host = host;
+            Client.EnableSsl = true;
+            Client.Credentials = new NetworkCredential(user, password);
+
+            MailMessage msg = new MailMessage();
+            msg.Sender = new MailAddress(from);
+            msg.From = new MailAddress(from);
+            foreach(String t in to)
+            {
+                msg.To.Add(t);
+            }
+            msg.Subject = subject;
+            msg.Body = message;
+            msg.IsBodyHtml = false;
+            msg.Priority = MailPriority.High;
+
+            Client.Send(msg);
+        }
+    }
+}
